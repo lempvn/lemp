1a0b0cae4e468ac04db0ae6ceb775689a4275ff3
Nhut <quocnhut.tn@gmail.com>
2024-10-28 16:04:41 +0700

    update code

--- install.sh
+++ install.sh
@@ -7,9 +7,50 @@
 #sleep 2
 #fi
 
-echo "Script se tu dong tat IPv6 de tien hanh cai dat Lemp, ban co the bat lai "
+# check xem IPv6 co dang bat hay khong, neu co se tat no
+echo "Script se tu dong tat IPv6 de tien hanh cai dat Lemp, ban co the bat lai sau khi cai xong LEMP"
+echo "Dang tien hanh kiem tra va tat IPv6 neu dang bat..."
+sleep 3
+echo "-----------------------------------------------------------------"
+# Kiem tra trang thai cua IPv6
+ipv6_status=$(sysctl net.ipv6.conf.all.disable_ipv6 | awk '{print $3}')
 
-# check xem IPv6 có đang bật hay không, nếu có sẽ tắt nó
+# Neu IPv6 dang bat (ipv6_status = 0), tat no
+if [ "$ipv6_status" -eq 0 ]; then
+    echo "IPv6 dang bat. Dang tat IPv6..."
+    # Vo hieu hoa IPv6
+    sysctl -w net.ipv6.conf.all.disable_ipv6=1
+    sysctl -w net.ipv6.conf.default.disable_ipv6=1
+    sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+    echo "IPv6 da duoc tat."
+    ipv6_status=1 # Cap nhat lai trang thai
+else
+    clear
+    echo "========================================================================="
+    echo "IPv6 da tat truoc do, khong can thay doi."
+fi
+
+# Cap nhat cau hinh sysctl de ap dung thay doi sau khi khoi dong lai
+if grep -q "net.ipv6.conf.all.disable_ipv6" /etc/sysctl.conf; then
+    # Neu da co dong nay thi thay doi gia tri
+    sed -i "s/net.ipv6.conf.all.disable_ipv6 = .*/net.ipv6.conf.all.disable_ipv6 = $ipv6_status/" /etc/sysctl.conf
+    sed -i "s/net.ipv6.conf.default.disable_ipv6 = .*/net.ipv6.conf.default.disable_ipv6 = $ipv6_status/" /etc/sysctl.conf
+    sed -i "s/net.ipv6.conf.lo.disable_ipv6 = .*/net.ipv6.conf.lo.disable_ipv6 = $ipv6_status/" /etc/sysctl.conf
+else
+    # Neu chua co thi them vao
+    echo "net.ipv6.conf.all.disable_ipv6 = $ipv6_status" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = $ipv6_status" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.lo.disable_ipv6 = $ipv6_status" >> /etc/sysctl.conf
+fi
+
+# Ap dung lai cac thiet lap sysctl ngay lap tuc
+sysctl -p > /dev/null 2>&1
+
+echo "Qua trinh tat IPv6 da hoan tat."
+echo "Tien hanh tai va cai dat LEMP"
+echo "-----------------------------------------------------------------"
+sleep 3
+
 
 
 sudo apt update
@@ -30,15 +71,7 @@ cd /opt/
 git clone https://github.com/lempvn/lemp.git vps_lemp
 
 
-#
-# Link tai goi cai dat lemp ve
-# them sau....
-# ....
-
-# chuyển file vps_lemp vừa tải ở trên vào thư mục /opt/vps_lemp bằng lệnh: mv vps_lemp /opt/vps_lemp và giải nén nó
-
-
-# chạy lần lượt các lệnh sau
+# chay lan luot cac lenh sau
 find /opt/vps_lemp/script/lemp/menu -type f -exec chmod 755 {} \;
 /opt/vps_lemp/script/lemp/menu/git-clone-done
 
